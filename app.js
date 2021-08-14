dcbb931b19ff07db144b807ac518603f40067a49
nelmish <standarduser@fitnessunchained.de>
2021-08-14 20:55:57 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+
+
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
