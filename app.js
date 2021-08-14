20a25ce2a2b2f7d0fb46ec1c069e0b642418b4b4
nelmish <standarduser@fitnessunchained.de>
2021-08-14 21:15:57 +0200

    73. Using a Helper Function for Navigation

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
@@ -8,12 +9,17 @@ const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({extended: true}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
+
+
+//module.exports = path.dirname(require.main.filename);
--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,19 @@
+const path = require('path');
+
 const express = require("express");
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
+// /admin/* ->
+
 router.get("/add-product", (req, res, next) => {
   console.log("In the middleware");
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>'
-  );
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post("/product", (req, res) => {
+router.post("/add-product", (req, res) => {
   console.log(req.body);
   res.redirect("/");
 });
