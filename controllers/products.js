16610e9bf422b11e838fd1fa34d2c22a6ede9f6e
nelmish <standarduser@fitnessunchained.de>
2021-08-16 20:28:59 +0200

    102. Fetching Data from Files Via the Model

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -11,18 +11,20 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res) => {
-  products.push({ title: req.body.title });
-  console.log(req.body);
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-    res.render('shop', {
+  Product.fetchAll((products) => {
+    res.render("shop", {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/',
+      pageTitle: "Shop",
+      path: "/",
       hasProducts: products.length > 0,
       activeShop: true,
-      productCSS: true
+      productCSS: true,
     });
-  };
+  });
+};
