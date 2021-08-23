143458bf34e127f6aeabaed347842906fbd34aaf
nelmish <standarduser@fitnessunchained.de>
2021-08-23 20:57:10 +0200

    165. Fetching Related Products

--- app.js
+++ app.js
@@ -1,25 +1,59 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 //app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
+const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
-app.use('/admin', adminRoutes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product); // will be set up automatically
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then((result) => {
+    return User.findByPk(1);
+    //console.log(result);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "admin", email: "admin@admin.com" });
+    }
+    return user; // == Promise.resolve(user)
+  })
+  .then((user) => {
+    //console.log(user);
+    app.listen(3000, () => {
+      console.log("Server listening on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
