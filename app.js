7704c462bd945573da1eea3b58de822ed0ca1f80
nelmish <standarduser@fitnessunchained.de>
2021-09-19 18:17:54 +0200

    241. Using MongoDB to Store Sessions

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const path = require("path");
 const express = require("express");
 
-const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+const MONGODB_URI = "mongodb://192.168.178.109:27017/shop?retryWrites=true";
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 //app.set('views', 'views');
@@ -12,11 +24,16 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
-const errorController = require("./controllers/error");
-const User = require("./models/user");
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 app.use((req, res, next) => {
   User.findById("6131150d391cc323ebdc3b2f")
@@ -34,7 +51,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect("mongodb://192.168.178.109:27017/shop?retryWrites=true")
+  .connect(MONGODB_URI)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,16 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").split(";")[4].trim().split("=")[1];
-  console.log(isLoggedIn);
+  //const isLoggedIn = req.get("Cookie").split(";")[4].trim().split("=")[1];
+  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: isLoggedIn,
+    isAuthenticated: false,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  //res.setHeader("Set-Cookie", "loggedIn=true; Max-Age=100");
+  req.session.isLoggedIn = true;
+  req.session.save();
   res.redirect("/");
 };
