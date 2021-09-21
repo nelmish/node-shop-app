9ecf2f7c403c5c89ef0242fbf1035f6b103cb9d2
nelmish <standarduser@fitnessunchained.de>
2021-09-21 21:10:57 +0200

    253. Our Updated Project Status

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,27 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-  //const isLoggedIn = req.get("Cookie").split(";")[4].trim().split("=")[1];
-  //console.log(req.session.isLoggedIn);
-  res.render("auth/login", {
-    path: "/login",
-    pageTitle: "Login",
-    isAuthenticated: false,
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('6131150d391cc323ebdc3b2f')
+  User.findById('5bab316ce0a7c75f783cb8a8')
     .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
-      req.session.save((err) => {
+      req.session.save(err => {
         console.log(err);
         res.redirect('/');
       });
@@ -23,9 +29,11 @@ exports.postLogin = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
+exports.postSignup = (req, res, next) => {};
+
 exports.postLogout = (req, res, next) => {
-  req.session.destroy((err) => {
-    console.log(err)
-    res.redirect("/");
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
   });
 };
